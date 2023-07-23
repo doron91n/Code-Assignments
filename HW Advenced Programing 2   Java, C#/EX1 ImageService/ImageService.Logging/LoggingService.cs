023fe382d838e504d96dcfa3fb7fcdf89baa8c8c
doron91n <139730676+doron91n@users.noreply.github.com>
2023-07-22 23:11:32 -0400

    Add files via upload

---
+++ HW Advenced Programing 2   Java, C#/EX1 ImageService/ImageService.Logging/LoggingService.cs
@@ -0,0 +1,43 @@
+﻿using ImageService.Logging.Modal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.ServiceProcess;
+
+/// <summary>
+/// LoggingService
+/// class that handles messages and passes them
+/// </summary>
+namespace ImageService.Logging
+{
+    public class LoggingService : ILoggingService
+    {
+		/// <summary>
+        /// LoggingService
+        /// constructor
+        /// </summary>
+        public LoggingService()
+        {
+        }
+		
+        public event EventHandler<MessageRecievedEventArgs> MessageRecieved;
+        
+		/// <summary>
+        /// Log
+        /// raises the message event and applies the message viewer
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="type"></param>
+		public void Log(string message, MessageTypeEnum type)
+        {
+            MessageRecievedEventArgs msg = new MessageRecievedEventArgs();
+            msg.Message = message;
+            msg.Status = type;
+            MessageRecieved?.Invoke(this, msg);
+        }
+    }
+}
+
---
+++ HW Advenced Programing 2   Java, C#/EX1 ImageService/ImageService.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using ImageService.Server;
+using ImageService.Controller;
+using ImageService.Modal;
+using ImageService.Logging;
+using ImageService.Logging.Modal;
+using System.Configuration;
+using ImageService.Infrastructure;
+using ImageService.Controller.Handlers;
+
+
+namespace ImageService
+{
+    #region Enum ServiceState
+    public enum ServiceState
+    {
+        SERVICE_STOPPED = 0x00000001,
+        SERVICE_START_PENDING = 0x00000002,
+        SERVICE_STOP_PENDING = 0x00000003,
+        SERVICE_RUNNING = 0x00000004,
+        SERVICE_CONTINUE_PENDING = 0x00000005,
+        SERVICE_PAUSE_PENDING = 0x00000006,
+        SERVICE_PAUSED = 0x00000007,
+    }
+    #endregion
+    #region struct ServiceStatus
+    [StructLayout(LayoutKind.Sequential)]
+    public struct ServiceStatus
+    {
+        public int dwServiceType;
+        public ServiceState dwCurrentState;
+        public int dwControlsAccepted;
+        public int dwWin32ExitCode;
+        public int dwServiceSpecificExitCode;
+        public int dwCheckPoint;
+        public int dwWaitHint;
+    };
+    #endregion
+	
+    public partial class ImageService : ServiceBase
+    {
+        #region members
+        private ImageServer m_imageServer;
+		private IImageServiceModal modal;
+		private IImageController controller;
+        private ILoggingService logging;
+        private EventLog eventLog;
+        private List<string> handlersPathList;
+        private int eventID = 1;
+        #endregion
+		
+		/// <summary>
+        /// ImageService
+        /// constructor
+        /// </summary>
+        /// <param name="args">arguments</param>
+        public ImageService(string[] args)
+        {
+            InitializeComponent();
+            string eventSourceName = ConfigurationManager.AppSettings["SourceName"];
+            string logName = ConfigurationManager.AppSettings["LogName"];
+            eventLog = new System.Diagnostics.EventLog();
+            if (!System.Diagnostics.EventLog.SourceExists(eventSourceName))
+            {
+                System.Diagnostics.EventLog.CreateEventSource(eventSourceName, logName);
+            }
+            eventLog.Source = eventSourceName;
+            eventLog.Log = logName;
+        }
+		
+        private void InitializeComponent()
+        {
+            this.eventLog = new System.Diagnostics.EventLog();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog)).BeginInit();
+            // 
+            // ImageService
+            // 
+            this.ServiceName = "ImageService";
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog)).EndInit();
+
+        }
+		
+        [DllImport("advapi32.dll", SetLastError = true)]
+        private static extern bool SetServiceStatus(IntPtr handle, ref ServiceStatus serviceStatus);
+
+
+        public void OnTimer(object sender, System.Timers.ElapsedEventArgs args)
+        {
+            //  monitoring activities .  
+            logging.Log("Monitoring the System", MessageTypeEnum.INFO);
+        }
+
+		/// <summary>
+        /// OnStart
+        /// initiallize the related classes and start running service
+        /// </summary>
+        /// <param name="args">arguments</param>
+        protected override void OnStart(string[] args)
+        {
+			//initiating members of LoggingService,ImageController,ImageServiceModal
+			logging = new LoggingService();
+			modal = new ImageServiceModal(ConfigurationManager.AppSettings["OutputDir"], int.Parse(ConfigurationManager.AppSettings["ThumbnailSize"]));
+			controller= new ImageController(modal);
+			handlersPathList =  ConfigurationManager.AppSettings["Handler"].Split(';').ToList();
+			//add function to event
+			logging.MessageRecieved += onMsg;
+			//initiating ImageServer
+			m_imageServer = new ImageServer(controller,logging,handlersPathList);
+			// Set up a timer to trigger every minute.  
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Interval = 60000; // 60 seconds  
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(this.OnTimer);
+            timer.Start();
+            logging.Log("Start", MessageTypeEnum.INFO);
+            // Update the service state to Start Pending. 
+            ServiceStatus serviceStatus = new ServiceStatus();
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_START_PENDING;
+            logging.Log("Start Pending", MessageTypeEnum.INFO);
+            serviceStatus.dwWaitHint = 100000;
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+            // Update the service state to Running.  
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_RUNNING;
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+        }
+		
+        protected override void OnContinue()
+        {
+            logging.Log("Continue", MessageTypeEnum.INFO);
+        }
+		
+        protected override void OnStop()
+        {
+			//send close command to server
+            m_imageServer.sendCommand();
+            logging.Log("Service OnStop", MessageTypeEnum.INFO);
+        }
+   
+		/// <summary>
+        /// OnMsg
+        /// gets the message to display and check the type of the message
+        /// </summary>
+        /// <param name="sender">the sender of the event</param>
+        /// <param name="e">message contains message and type</param>
+        public void onMsg(object sender, Logging.Modal.MessageRecievedEventArgs e)
+        {
+            EventLogEntryType eventType;
+            switch (e.Status)
+            {
+                case MessageTypeEnum.INFO:
+                    eventType = EventLogEntryType.Information;
+                    break;
+                case MessageTypeEnum.WARNING:
+                    eventType = EventLogEntryType.Warning;
+                    break;
+                case MessageTypeEnum.FAIL:
+                    eventType = EventLogEntryType.FailureAudit;
+                    break;
+                default:
+                    eventType = EventLogEntryType.Information;
+                    break;
+            }
+            eventLog.WriteEntry(e.Message, eventType,eventID++);
+        }
+    }
+}
