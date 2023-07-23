c0dded4b2516cf7d85c2fcda500a175b385d1383
doron91n <139730676+doron91n@users.noreply.github.com>
2023-07-22 23:14:47 -0400

    Add files via upload

---
+++ HW Advenced Programing 2   Java, C#/EX3 ImageService with Web Page c#/Commands.Infrastructure c#/Event/CommandRecievedEventArgs.cs
@@ -0,0 +1,78 @@
+﻿
+using System;
+
+namespace Commands.Infrastructure
+{
+    public class CommandRecievedEventArgs : EventArgs
+    {
+        public CommandEnum CommandID { get; set; }      // The Command ID
+        public string CommandName { get
+            {
+                string name = "";
+                switch (CommandID)
+                {
+                    case CommandEnum.NewFileCommand:
+                        name = "NewFileCommand";
+                        break;
+                    case CommandEnum.GetConfigCommand:
+                        name = "GetConfigCommand";
+                        break;
+                    case CommandEnum.LogCommand:
+                        name = "LogCommand";
+                        break;
+                    case CommandEnum.CloseCommand:
+                        name = "CloseCommand";
+                        break;
+                     default:
+                        name = "NoCommand";
+                        break;
+                }
+                return name;
+            } }                  // the command name
+
+        public string[] Args { get; set; }          // the provided arguments
+        public string RequestDirPath { get; set; }  // The Request Directory
+
+        /// <summary>
+        /// Function Name: CommandRecievedEventArgs()
+        /// Description: constructor.
+        /// Arguments: id: this command id enum.
+        ///            args: provided string arguments for this command.
+        ///            path: string the command path if needed.
+        /// </summary>
+        /// <returns> null. </returns>
+        public CommandRecievedEventArgs(CommandEnum id, string[] args, string path)
+        {
+            CommandID = id;
+            Args = args;
+            RequestDirPath = path;
+        }
+        /// <summary>
+        /// Function Name: ToString()  OverRides Object.TOString()
+        /// Description: returns a string representing this class.
+        /// Arguments: null
+        /// </summary>
+        /// <returns> string represnting this class. </returns>
+        public override string  ToString () {
+
+            return "Command: " + CommandName + " Args: " + ArgsToString() + " Path: " + RequestDirPath;
+        }
+        /// <summary>
+        /// Function Name: ArgsToString()
+        /// Description: returns a string representing this class members.
+        /// Arguments: null
+        /// </summary>
+        /// <returns> string filled with this class members content </returns>
+        private string ArgsToString()
+        {
+            string s = "";
+            int i = 0;
+            foreach (string arg in Args)
+            {
+                s += " [" + i + "]= " + arg;
+                i++;
+            }
+            return s;
+        }
+    }
+}
---
+++ HW Advenced Programing 2   Java, C#/EX3 ImageService with Web Page c#/Commands.Infrastructure c#/Modal/MessageRecievedEventArgs.cs
@@ -0,0 +1,32 @@
+﻿
+
+using System;
+namespace Commands.Infrastructure
+{
+    public class MessageRecievedEventArgs : EventArgs
+    {
+        public MessageTypeEnum Status { get; set; }
+        public string StatusName { get
+            {
+                string name = "";
+                switch (Status)
+                {
+                    case MessageTypeEnum.INFO:
+                        name = "INFO";
+                        break;
+                    case MessageTypeEnum.FAIL:
+                        name = "FAIL";
+                        break;
+                    case MessageTypeEnum.WARNING:
+                        name = "WARNING";
+                        break;
+                    default:
+                        name = "No Type";
+                        break;
+                }
+                return name;
+            }
+        }
+        public string Message { get; set; }
+    }
+}
